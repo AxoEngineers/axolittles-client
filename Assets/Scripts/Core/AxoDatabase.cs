9d5a8efe4d40901efa8f287de3846957e619e2da
aleks <>
2021-12-30 22:48:37 +1100

    Import traits from database

---
+++ Assets/Scripts/Core/AxoDatabase.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using UnityEngine;
+
+public class AxoDatabase : MonoBehaviour
+{
+    public AxoStruct[] db;
+    
+    private void Start()
+    {
+        var arr = JsonUtility.FromJson<AxoArray>("{\"data\":" + Resources.Load<TextAsset>("axotraitsdb").text + "}");
+        db = arr.data;
+    }
+}
---
+++ Assets/Scripts/Data/AxoStruct.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+[Serializable]
+public class AxoStruct
+{
+    public string id;
+    public string background;
+    public string top;
+    public string face;
+    public string outfit;
+    public string type;
+}
+
+[Serializable]
+public class AxoArray
+{
+    public AxoStruct[] data;
+}
