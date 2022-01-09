f959d7378dd645f78dffdf8820cf1eea352eedb3
aleks <>
2022-01-09 18:38:57 +1100

    Optimize asset imports

--- Assets/Scripts/Networking/SocketClient.cs
+++ Assets/Scripts/Networking/SocketClient.cs
@@ -1,11 +1,5 @@
-using System;
-using System.Collections;
-using System.Collections.Generic;
 using System.Runtime.InteropServices;
-using Infteract;
 using UnityEngine;
-using UnityEngine.Events;
-using UnityEngine.Networking;
 
 namespace LwNetworking
 {
