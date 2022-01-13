02405d97a1bea5045448fb9524f3bb4147a8ccb0
Zeus <cody.stoeckel@gmail.com>
2022-01-13 18:27:01 +1030

    Added initial active axo UI

--- Assets/Scripts/Entity/ActiveAxoManager.cs
+++ Assets/Scripts/Entity/ActiveAxoManager.cs
@@ -7,10 +7,13 @@ using UnityEngine.EventSystems;
 public class ActiveAxoManager : Mingleton<ActiveAxoManager>
 {
     private EventTrigger eventTrigger;
-
-    private int maxActive = 5;
+    
     private List<AxoInfo> active;
     private bool generating;
+    
+    private int maxActive => gridItems.Length;
+
+    public AvatarGridElement[] gridItems;
     
     new void Awake()
     {
@@ -26,6 +29,7 @@ public class ActiveAxoManager : Mingleton<ActiveAxoManager>
         {
             existing.gameObject.SetActive(false);
             active.Remove(existing);
+            RefreshGrid();
             return;
         }
         
@@ -37,10 +41,27 @@ public class ActiveAxoManager : Mingleton<ActiveAxoManager>
                 AxoModelGenerator.Instance.Generate(id, axoObject =>
                 {
                     active.Add(axoObject);
+                    RefreshGrid();
                     axoObject.gameObject.SetActive(true);
                     generating = false;
                 });
             }
+        }
+    }
+
+    private void RefreshGrid()
+    {
+        for (int i = 0; i < maxActive; i++)
+        {
+            if (i >= active.Count)
+            {
+                gridItems[i].gameObject.SetActive(false);
+                continue;
+            }
+
+            gridItems[i].Text.text = $"{active[i].id}";
+            gridItems[i].Icon.sprite = AvatarGrid.Instance.spriteCache[active[i].id];
+            gridItems[i].gameObject.SetActive(true);
         }
     }
 }
